338bae86094a600607e03162f701f93f733df3d7
Bilal-Alqtrawi <bbill4552@gmail.com>
2024-11-21 15:49:32 +0200

    update on css file and on navbar component

--- src/componets/Navbar.js
+++ src/componets/Navbar.js
@@ -8,35 +8,36 @@ const Navbar = ({ users: { isAuthenticated }, logout }) => {
   const links = (
     <ul>
       <li>
-          <Link to="/login">Login</Link>
+        <Link to="/login">Login</Link>
       </li>
     </ul>
   );
   const authLinks = (
     <ul>
       <li>
-          <Link onClick={logout} to="/">Logout</Link>
+        <Link onClick={logout} to="/">
+          Logout
+        </Link>
       </li>
     </ul>
   );
   const onClick = (e) => {
     document.querySelector(".sidebar").classList.add("active");
-  }
+  };
   return (
     <nav className="navbar bg-navbar">
       <h1>
-        <Link className="logo-navbar" to="/">
-          TawaSol
-        </Link>
-        <button type="button" className="menu-bar" onClick={onClick}>
+        {isAuthenticated ? (
+          <button type="button" className="menu-bar" onClick={onClick}>
             <i class="fas fa-bars"></i>
-        </button>
+          </button>
+        ) : (
+          <Link className="logo-navbar" to="/">
+            TawaSol
+          </Link>
+        )}
       </h1>
-      <Fragment>
-        {
-            isAuthenticated ? authLinks : links
-        }
-      </Fragment>
+      <Fragment>{isAuthenticated ? authLinks : links}</Fragment>
     </nav>
   );
 };
